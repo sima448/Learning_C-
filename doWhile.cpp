34cd00c301abef116d98e3081264a97e12f445ed
Sima-Kumari <simak4484@gmail.com>
2026-01-15 23:05:41 +0530

    Add files via upload

---
+++ doWhile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int i = 1;// Initialize i to 1
+    int i = 11;
+    do{
+        cout<<i<<" ";
+        i++;
+    }
+    while(i<=10);
+
+
+
+    
+}
